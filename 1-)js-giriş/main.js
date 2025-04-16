872c5b6ab37697d8ee0981737c9eb792493e186c
yusufyaman07 <yamanyusuf00000@gmail.com>
2025-04-17 00:02:45 +0300

    first commit

---
+++ 1-)js-giriş/main.js
@@ -0,0 +1,20 @@
+/*
+* Js Nedir?
+
+- Javascript günümüzde çokca kullanılan bir programlama dilidir.
+- Bir dilin programalama dili olmasını dinamik işlemler yapıp yapmamasına göre belirleriz.(Html  2+2 sonucununda 4 döndürmez fakat javascript 2+2=4 sonucunu döndürür.Bu sebeple javascript bir programlama diliyken html bir programlama dili değilir.)
+- Javascript kodlamaları .js uzantılı dosyalarda yapılır.Html dosyasına javascript dosyası bağlamak için script etiketi kullanılır.Bu etiketin src attribute'üne bağlamak istediğimiz js dosyasının adını yazarak gerekli bağlantıyı kurarız.
+
+*/
+
+// * Javascript Çıktı Metotları
+
+// 1-) document.write(): Html kısmındaki body'nin javascript'deki karşılığı document'dir.document.write komutu sayesinde bu alana çıktı verebiliriz.
+
+document.write("Javascript dersinden selamlar");
+
+// 2-) console.log(): Console tarayıcıda sağ tık > incele kısmına gidilince açılan bir menüdür.console.log() ile yazılan çıktılar bu kısma düşer.Bu metodu veri kontrolü,hata tespiti ve bağlantı kontrolü gibi durumlarda çokça kullanacağız.
+
+console.log("Hello World");
+
+// 3-) alert():
